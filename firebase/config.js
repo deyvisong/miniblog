4f7b7ca65419954eca0af5193d292dea34c3b63c
cd ~/code/$deyvisong/dotfiles && zsh git_setup.sh <deyvisonalves@protonmail.com>
2025-01-09 20:24:14 +0100

    miniblog project with firebase

---
+++ firebase/config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyC-5bgDJDH6190ppEvxOvUSwHtRxoXpMbg",
+  authDomain: "miniblog-c6d6e.firebaseapp.com",
+  projectId: "miniblog-c6d6e",
+  storageBucket: "miniblog-c6d6e.firebasestorage.app",
+  messagingSenderId: "304035660080",
+  appId: "1:304035660080:web:3d31aa1e16c532b379c8f5",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
