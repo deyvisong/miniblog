c055adeb9581334fe64a5c614ae405e2d3d81428
cd ~/code/$deyvisong/dotfiles && zsh git_setup.sh <deyvisonalves@protonmail.com>
2025-01-09 21:41:30 +0100

    create pages, Routes etc

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+const Footer = () => {
+  return (
+    <footer>
+      <h3>Write about your interests</h3>
+      <p>Mini Blog &copy; 2025</p>
+    </footer>
+  );
+};
+
+export default Footer;
