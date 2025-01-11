ec921f043b689e1d3bb93c684b560badea18d970
cd ~/code/$deyvisong/dotfiles && zsh git_setup.sh <deyvisonalves@protonmail.com>
2025-01-11 23:17:49 +0100

    insert document firebase

--- src/pages/CreatePost/CreatePost.js
+++ src/pages/CreatePost/CreatePost.js
@@ -1,8 +1,79 @@
 import styles from "./CreatePost.module.css";
+
+import { useState } from "react";
+import { useNativate } from "react-router-dom";
+import { useAuthValue } from "../../context/AuthContext";
+import { useAuthentication } from "../../hooks/useAuthentication";
+
 const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [body, setBody] = useState("");
+  const [tags, setTags] = useState([]);
+  const [formError, setFormError] = useState("");
+  const { loading } = useAuthentication;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>Create Post</h1>
+    <div className={styles.create_post}>
+      <h2>Create Post</h2>
+      <p>Write about anything you want and share your knowledge.</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Title:</span>
+          <input
+            type="text"
+            name="title"
+            required
+            placeholder="Think about your title..."
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
+        </label>
+        <label>
+          <span>Image URL:</span>
+          <input
+            type="text"
+            name="image"
+            required
+            placeholder="Insert an image that represents your post"
+            onChange={(e) => setImage(e.target.value)}
+            value={image}
+          />
+        </label>
+        <label>
+          <span>Post Content:</span>
+          <textarea
+            name="body"
+            required
+            placeholder="Share your thoughts here"
+            onChange={(e) => setBody(e.target.value)}
+            value={body}
+          ></textarea>
+        </label>
+        <label>
+          <span>Tags:</span>
+          <input
+            type="text"
+            name="tags"
+            required
+            placeholder="Add your tags, separated by commas"
+            onChange={(e) => setTags(e.target.value)}
+            value={tags}
+          />
+        </label>
+        <button className="btn">Post</button>
+        {/* {!loading && <button className="btn">Sign Up</button>}
+        {loading && (
+          <button className="btn" disabled>
+            Creating...
+          </button>
+        )}
+        {formError && <p className="error">{formError}</p>} */}
+      </form>
     </div>
   );
 };
